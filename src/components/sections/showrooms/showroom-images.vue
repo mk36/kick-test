6ab0eaa3c6cd41316365523beba46a6e0f5c93d6
mk36 <mikeking36@hotmail.co.uk>
2020-04-28 10:48:26 +0100

    solved grid image issue

--- src/components/sections/showrooms/showroom-images.vue
+++ src/components/sections/showrooms/showroom-images.vue
@@ -2,33 +2,61 @@
     .showroom-images-section{
         .showroom-images{
             @include flex(100%, flex, unset, unset);
+            $image-padding: 25px;
+
             &__row{
                 display: flex;
                 flex-direction: column;
-                max-height: 500px;
-                overflow: hidden;
+                max-height: 700px;
+
+                img{
+                    object-fit: cover;
+                }
 
                 &.left-row{
-                    flex-basis: 25%;
+                    flex-basis: 45%;
+                    @include paddingBorderBox(0 $image-padding 0 0);
 
                     img{
-                        flex-basis: 50%;
+                        &:first-child{
+                            flex-basis: 35%;
+                            @include paddingBorderBox(0 0 $image-padding 0);
+                        }
+
+                        &:nth-child(2){
+                            flex-grow: 1;
+                        }
                     }
                 }
 
                 &.right-row{
-                    flex-basis: 75%;
+                    flex-basis: 65%;
 
-                    img:nth-child(1){
+                    .first-image{
                         flex-basis: 30%;
+                        max-height: 40%;
+                        @include paddingBorderBox(0 0 $image-padding 0);
                     }
 
                     .right-row__images{
                         flex-basis: 70%;
                         display: flex;
 
-                        div{
+                        .first-nested-image{
+                            @include paddingBorderBox(0 $image-padding 0 0);
+                        }
 
+                        img, &__nested{
+                            flex-basis: 50%;
+                        }
+
+                        &__nested{
+                            @include flex(100%, flex, center, center);
+                            flex-direction: column;
+
+                            img:nth-child(1){
+                                @include paddingBorderBox(0 0 $image-padding 0);
+                            }
                         }
                     }
                 }
@@ -47,12 +75,12 @@
                 </div>
 
                 <div class="showroom-images__row right-row">
-                    <img :src="getImgUrl('images/showroom2.jpg')" alt="" class="showroom-images__row__img">
+                    <img :src="getImgUrl('images/showroom2.jpg')" alt="" class="showroom-images__row__img first-image">
 
                     <div class="right-row__images">
-                        <img :src="getImgUrl('images/showroom-4.jpg')" alt="" class="showroom-images__row__img">
+                        <img :src="getImgUrl('images/showroom-4.jpg')" alt="" class="showroom-images__row__img first-nested-image">
 
-                        <div>
+                        <div class="right-row__images__nested">
                             <img :src="getImgUrl('images/showroom_5.jpg')" alt="" class="showroom-images__row__img">
                             <img :src="getImgUrl('images/showroom6.jpg')" alt="" class="showroom-images__row__img">
                         </div>
